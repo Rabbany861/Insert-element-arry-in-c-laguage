034bc0e59093178045c7b7ef524629c0e76f1059
Rabbany861 <rabbanyislam861@gmail.com>
2024-11-23 13:41:59 +0600

    Add files via upload

--- Insertion and deletion operation in c language.cpp
+++ Insertion and deletion operation in c language.cpp
@@ -16,28 +16,28 @@ int main()
     }
 
     int position;
-    printf("Insertion Position : ");
+    printf("Deletion Position : ");
     scanf("%d",&position);
 
 
 
-    int key_value;
-    printf("Insertion value : ");
-    scanf("%d",&key_value);
+    //int key_value;
+    //printf("Insertion value : ");
+    //scanf("%d",&key_value);
 
-    printf("the arry element before insertion : ");
+    printf("the arry element before deletion : ");
     for(int i=0; i<n; i++)
     {
         printf("%d ",arr[i]);
     }
     printf("\n");
-    for(int i=n-1; i>=position-1; i--)
+    for(int i=position-1; i<n; i++)
     {
-        arr[i+1]=arr[i];
+        arr[i]=arr[i+1];
     }
-    arr[position-1]=key_value;
-    printf("The arry element after insertion : ");
-    for(int i=0; i<n+1; i++)
+   // arr[position-1]=key_value;
+    printf("The arry element after deletion : ");
+    for(int i=0; i<n-1; i++)
     {
         printf("%d ",arr[i]);
     }
